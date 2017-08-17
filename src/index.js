1107715f9d7281f93657e8a8922a589b4bfc1ae6
Jonas Könning <jonaswk@users.noreply.github.com>
2017-08-17 16:06:55 +0200

    Login implemented and tested

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 // var XMLHttp = require('xmlhttprequest').XMLHttpRequest;
+// eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOlsiMjQyYzQwODQtM
+// zUzYS00ODVkLTlkM2MtODBmNTVhZDczZTIwIl0sImlzcyI6Imh0dHBzOlwvXC9h
+// cGkubGVhcm5pbmctbGF5ZXJzLmV1XC9vXC9vYXV0aDJcLyIsImV4cCI6MTUwMj
+// k3OTE4MSwiaWF0IjoxNTAyOTc1NTgxLCJqdGkiOiIyZTkyNWQ4ZS1lYWQ2LTRkZWE
+// tOGQ2Mi1mODhjNWFjZjY5MmEifQ.u3LOCZtmL0sdTjxLZYrc_StZbdVW91SB-5GX2XS
+// tAsLb-E1GcOjC6beIrHdeWtEXYohWbh3IOx10MEeT6Pog-yrOsVhETpwxlbAYkF
+// gV8uAm44EYSGOh0TTcj5y43T-FqLIyT8MxZSOkUNa4oIDOhFITQQqxzI5eQrlMO
+// F6tPSg
 var axios = require('axios');
 
 export default class RoleApiJS {
@@ -17,17 +25,20 @@ export default class RoleApiJS {
     return (a['y'] < b['y'] ? -1 : 1);
   }
 
-  testRequest() {
-    axios.get('http://example.com/')
+  login() {
+    const instance = axios.create({
+      headers: {'access_token': this.token}
+    });
+
+    instance.get(this.url + 'o/oauth2/authorizeImplicit?userinfo_endpoint=https://api.learning-layers.eu/o/oauth2/userinfo')
       .then(function (response) {
-        console.log(response);
+        this.cookie = response.headers['set-cookie'][0].split(';')[0].split('=')[1];
+        return true;
       })
       .catch(function (error) {
         console.log(error);
+        return false;
       });
-  }
-  login() {
-
   }
 
   loginCookie() {
